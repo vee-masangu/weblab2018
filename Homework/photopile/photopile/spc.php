c034f3fef9875eb3c806bb2b87b8c075b5e4844d
Masangu <vim11@w204607mac.local>
2018-04-19 10:55:48 -0400

    vacation assignment and layout assignment but not done yet

---
+++ Homework/photopile/photopile/spc.php
@@ -0,0 +1,185 @@
+<?php
+
+// Steemit Photo Challenge Demo
+
+require_once '../config.php';
+define('PAGE', 'demo');
+require_once '../header.php';
+?>
+
+<div class="row">
+	<div class="col-sm-12">
+		<h1 class="h4">Steemit Photo Challenge 1st Place Winners</h1>
+
+		<div class="photopile-wrapper spc-wrapper">
+			<ul class="photopile">
+
+				<!-- SPC 1 -->
+				<li>
+				<a href="images/fullsize/spc01.jpg">
+				<img src="images/thumbs/spc01.jpg" alt="SPC #1 Animal Portraits - @sharker" width="144" height="100" />
+				</a>
+				</li>
+
+				<!-- SPC 2 -->
+				<li>
+				<a href="images/fullsize/spc02.jpg">
+				<img src="images/thumbs/spc02.jpg" alt="SPC #2 Shadows - @theyam" width="145" height="94" />
+				</a>
+				</li>
+
+				<!-- SPC 3 -->
+				<li>
+				<a href="images/fullsize/spc03.jpg">
+				<img src="images/thumbs/spc03.jpg" alt="SPC #3 Desolation - @pkattera" width="140" height="99" />
+				</a>
+				</li>
+
+				<!-- SPC 4 -->
+				<li>
+				<a href="images/fullsize/spc04.jpg">
+				<img src="images/thumbs/spc04.jpg" alt="SPC #4 Nature Closeup - @mweich" width="144" height="98" />
+				</a>
+				</li>
+
+				<!-- SPC 5 -->
+				<li>
+				<a href="images/fullsize/spc05.jpg">
+				<img src="images/thumbs/spc05.jpg" alt="SPC #5 Emotive Human Portrait - @dercoco" width="145" height="97" />
+				</a>
+				</li>
+
+				<!-- SPC 6 -->
+				<li>
+				<a href="images/fullsize/spc06.jpg">
+				<img src="images/thumbs/spc06.jpg" alt="SPC #6 Natural Landscapes - @stansmith" width="145" height="97" />
+				</a>
+				</li>
+
+				<!-- SPC 7 -->
+				<li>
+				<a href="images/fullsize/spc07.jpg">
+				<img src="images/thumbs/spc07.jpg" alt="SPC #7 Architecture - @shieha" width="150" height="87" />
+				</a>
+				</li>
+
+				<!-- SPC 8 -->
+				<li>
+				<a href="images/fullsize/spc08.jpg">
+				<img src="images/thumbs/spc08.jpg" alt="SPC #8 Long Exposure - @stansmith" width="145" height="97" />
+				</a>
+				</li>
+
+				<!-- SPC 9 -->
+				<li>
+				<a href="images/fullsize/spc09.jpg">
+				<img src="images/thumbs/spc09.jpg" alt="SPC #9 Action - @kranartz" width="145" height="97" />
+				</a>
+				</li>
+
+				<!-- SPC 10 -->
+				<li>
+				<a href="images/fullsize/spc10.jpg">
+				<img src="images/thumbs/spc10.jpg" alt="SPC #10 Minimalism - @xroni" width="147" height="97" />
+				</a>
+				</li>
+
+				<!-- SPC 11 -->
+				<li>
+				<a href="images/fullsize/spc11.jpg">
+				<img src="images/thumbs/spc11.jpg" alt="SPC #11 Glamour and Fashion - @condra" width="67" height="100" />
+				</a>
+				</li>
+
+				<!-- SPC 12 -->
+				<li>
+				<a href="images/fullsize/spc12.jpg">
+				<img src="images/thumbs/spc12.jpg" alt="SPC #12 The Soul of the City - @dek" width="152" height="82" />
+				</a>
+				</li>
+
+				<!-- SPC 13 -->
+				<li>
+				<a href="images/fullsize/spc13.jpg">
+				<img src="images/thumbs/spc13.jpg" alt="SPC #13 Hands - @siavach" width="150" height="100" />
+				</a>
+				</li>
+
+				<!-- SPC 14 -->
+				<li>
+				<a href="images/fullsize/spc14.jpg">
+				<img src="images/thumbs/spc14.jpg" alt="SPC #14 Autumn Foliage - @romancs" width="150" height="100" />
+				</a>
+				</li>
+
+				<!-- SPC 15 -->
+				<li>
+				<a href="images/fullsize/spc15.jpg">
+				<img src="images/thumbs/spc15.jpg" alt="SPC #15 Spiderwebs - @yetaras" width="140" height="105" />
+				</a>
+				</li>
+
+				<!-- SPC 16 -->
+				<li>
+				<a href="images/fullsize/spc16.jpg">
+				<img src="images/thumbs/spc16.jpg" alt="SPC #16 Food - @avtzis.petros" width="150" height="100" />
+				</a>
+				</li>
+
+				<!-- SPC 17 -->
+				<li>
+				<a href="images/fullsize/spc17.jpg">
+				<img src="images/thumbs/spc17.jpg" alt="SPC #17 Countryside - @sharker" width="208" height="95" />
+				</a>
+				</li>
+
+				<!-- SPC 18 -->
+				<li>
+				<a href="images/fullsize/spc18.jpg">
+				<img src="images/thumbs/spc18.jpg" alt="SPC #18 Transport - @jang" width="149" height="99" />
+				</a>
+				</li>
+
+				<!-- SPC 19 -->
+				<li>
+				<a href="images/fullsize/spc19.jpg">
+				<img src="images/thumbs/spc19.jpg" alt="SPC #19 Human Body - @dreamstream" width="116" height="100" />
+				</a>
+				</li>
+
+				<!-- SPC 20 -->
+				<li>
+				<a href="images/fullsize/spc20.jpg">
+				<img src="images/thumbs/spc20.jpg" alt="SPC #20 Sunrise Sunset - @unhorsepower777" width="150" height="100" />
+				</a>
+				</li>
+
+				<!-- SPC 21 -->
+				<li>
+				<a href="images/fullsize/spc21.jpg">
+				<img src="images/thumbs/spc21.jpg" alt="SPC #21 Reflections - @okkiedot" width="150" height="101" />
+				</a>
+				</li>
+
+				<!-- SPC 22 -->
+				<li>
+				<a href="images/fullsize/spc22.jpg">
+				<img src="images/thumbs/spc22.jpg" alt="SPC #22 Sacred - @sharker" width="139" height="104" />
+				</a>
+				</li>
+
+				<!-- SPC 23 -->
+				<li>
+				<a href="images/fullsize/spc23.jpg">
+				<img src="images/thumbs/spc23.jpg" alt="SPC #23 Nostalgia - @@jay-kopinski" width="168" height="99" />
+				</a>
+				</li>
+
+			</ul>
+		</div>
+	</div>
+</div> <!-- row -->
+
+<?php
+require_once '../footer.php';
+?>
